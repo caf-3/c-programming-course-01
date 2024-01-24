b7d0a21acf6c8c0b483616127c1f5f24881127b8
caf-3 <00caf3@gmail.com>
2024-01-24 09:39:17 +0200

    temperature converter

---
+++ 13-temperature-converter.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+
+    char unit;
+    float temp;
+
+    printf("\nIs the emperature in (F) or (C)?: ");
+    scanf("%c", &unit);
+    unit = toupper(unit);
+    if(unit == 'C') {
+        // printf("The temp is currently in C.\n");
+        printf("\nEnter the temperature in Celsius: ");
+        scanf("%f", &temp);
+        temp = (temp * 9 /5) + 32;
+        printf("\nThe temperature in Farenheit is: %.1f\n", temp);
+    }else if(unit == 'F') {
+        printf("The temp is currently in F.\n");
+    }else {
+        printf("%c is not a valid unit of measurement\n", unit);
+    }
+    
+
+    return 0;
+}
